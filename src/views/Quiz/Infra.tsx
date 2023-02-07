5553451360a0ad877d2c7b2bb8a6015958312fe2
Saber-grou <111590089+Kevin-Locatelli@users.noreply.github.com>
2023-02-07 11:13:38 +0100

    Update Infra.tsx

--- src/views/Quiz/Infra.tsx
+++ src/views/Quiz/Infra.tsx
@@ -7,9 +7,9 @@ const QuizIntra = () => {
             questions={[
                 {
                     title: "Challenge Infra",
-                    description: "Vous devez vous connectez sur une interface Web afin de récupérer un drapeau, vous avez à votre dispositions un switch avec des VLANs configurer, votre administrateur réseau n’étant pas très compétent, à vous de rajouter le VLAN permettant de vous connecter à ce serveur ou d’enlever un VLAN, vous pouvez utiliser le port qui est libre pour débranchez votre câble et initier la connexion avec le nouveau vlan une fois associer.\n" +
-                        "Authentification : ch48h ch48h" +
-                        "Quel est le drapeau (mot de passe) pour florence ?",
+                    description: "Connectez-vous à une interface Web pour récupérer un drapeau en utilisant un switch avec des VLANs configurés. Modifiez le VLAN pour vous connecter au serveur ou enlevez-en un. Utilisez un port libre pour débrancher et reconnecter avec le nouveau VLAN." +
+                        " Quel est le drapeau (mot de passe) pour Florence ?"+
+                        " Authentification : ch48h ch48h.",
                     answer : "462m6rYWq5YRmh",
                 }
             ]}
