7db19ed569674dd9ffad938e8e2f34655edb7f51
Maximilien Bey <bey.maximilien@gmail.com>
2023-02-07 11:23:02 +0100

    merge main

--- src/views/Quiz/Infra.tsx
+++ src/views/Quiz/Infra.tsx
@@ -1,20 +1,21 @@
 import Quiz from "../../components/Quiz"
 
 const QuizIntra = () => {
-    return (
-        <Quiz
-            title="Infra"
-            questions={[
-                {
-                    title: "Challenge Infra",
-                    description: "Connectez-vous à une interface Web pour récupérer un drapeau en utilisant un switch avec des VLANs configurés. Modifiez le VLAN pour vous connecter au serveur ou enlevez-en un. Utilisez un port libre pour débrancher et reconnecter avec le nouveau VLAN." +
-                        " Quel est le drapeau (mot de passe) pour Florence ?"+
-                        " Authentification : ch48h ch48h.",
-                    answer : "462m6rYWq5YRmh",
-                }
-            ]}
-        />
-    )
+  return (
+    <Quiz
+      title="Infra"
+      questions={[
+        {
+          title: "Challenge Infra",
+          description:
+            "Connectez-vous à une interface Web pour récupérer un drapeau en utilisant un switch avec des VLANs configurés. Modifiez le VLAN pour vous connecter au serveur ou enlevez-en un. Utilisez un port libre pour débrancher et reconnecter avec le nouveau VLAN." +
+            " Quel est le drapeau (mot de passe) pour Florence ?" +
+            " Authentification : ch48h ch48h.",
+          answer: "462m6rYWq5YRmh",
+        },
+      ]}
+    />
+  )
 }
 
 export default QuizIntra
